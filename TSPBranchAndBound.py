129b06c9670be2eb7e0b7fff89de4745411b50a4
Jenna Atkinson <>
2022-11-28 18:15:50 -0700

    Add State class and test

---
+++ TSPBranchAndBound.py
@@ -0,0 +1,184 @@
+import math
+
+from TSPClasses import City
+
+
+INF_STRING = "-"
+
+# Given a list of cities, return an init state
+def generateRootStateFromCities(cities):
+	#Start with the first city as "visited"
+	state = State(unvisitedSet=set(cities), cities=cities)
+
+	#Initialize the matrix
+	matrix = dict()
+	for city in cities:
+		for otherCity in cities:
+			if city != otherCity:
+				cost = city.costTo(otherCity)
+				if cost != math.inf:
+					matrix[tuple((city._index, otherCity._index))] = cost
+	state.matrix = matrix
+	state.reduceCostOnMatrix()
+	print(state.matrix)
+	return state
+
+class State:
+	#unvisitedCitiesSet, set of cities that haven't been visited yet
+	#matrix, dictionary to show which edges can be used next
+	#cities, reference to the list of cities
+	#routeSoFar, list of cities in order of what we have visited so far
+	#costSoFar, the cost we have accumulated on our route
+	def __init__(self, unvisitedSet=set(), matrix=dict(), cities=[], route=[], costSoFar:int=0):
+		self.unvisitedCitiesSet = unvisitedSet
+		self.matrix = matrix
+		self.cities = cities
+		self.routeSoFar = route
+		self.costSoFar = costSoFar
+
+	# Sees if this State yields a solution
+	def isSolution(self) -> bool:
+		return len(self.unvisitedCitiesSet) == 0 and self.costSoFar != math.inf
+
+	# Return the route if a solution
+	def getSolutionRoute(self):
+		if self.isSolution():
+			return self.routeSoFar
+		else:
+			return None
+
+	def isVisitedCity(self, city:City) -> bool:
+		return not (city in self.unvisitedCitiesSet)
+
+	# TODO: Not sure if this will work with the breaking??
+	# def iterateMatrix(self, row:bool, doThing):
+	# # Look at rows to find the minCost
+	# 	for rowIndex in range(len(self.cities)):
+	# 		# If the whole row is going to be infinities, move on
+	# 		if (self.isVisitedCity(self.cities[rowIndex])):
+	# 			continue
+	# 		minCost = math.inf
+	# 		for colIndex in range(len(self.cities)):
+	# 			if row:
+	# 				cell = tuple((self.cities[rowIndex], self.cities[colIndex]))
+	# 			else: # Rows and columns are reversed here
+	# 				cell = tuple((self.cities[colIndex], self.cities[rowIndex]))
+	# 			cost = self.matrix.get(cell)
+	# 			if cost != None:
+	# 				doThing(minCost)
+
+	# Ensures at least one zero on the rows/columns of the matrix (besides inf)
+	# Note: comments/names are the to show rows, but it reverses for columns
+	def findMinCostAndNormalize(self, row:bool=True):
+		# Look at rows to find the minCost
+		for rowIndex in range(len(self.cities)):
+			# If the whole row is going to be infinities, move on
+			if (self.isVisitedCity(self.cities[rowIndex])):
+				continue
+			
+			minCost = math.inf
+
+			#Examine each cell to find the minCost
+			for colIndex in range(len(self.cities)):
+				if row:
+					cell = tuple((rowIndex, colIndex))
+				else: # Rows and columns are reversed here
+					cell = tuple((colIndex, rowIndex))
+				
+				cost = self.matrix.get(cell)
+				if cost != None and cost < minCost:
+					minCost = cost
+					if minCost == 0:
+						break
+			
+			# If we are taking on additional costs, we need to readjust the costSoFar and row/column numbers
+			if minCost != 0 and minCost < math.inf:
+				self.costSoFar += minCost
+
+			# Update all costs in same row/column to normalize
+				for colIndex in range(len(self.cities)):
+					if row:
+						cell = tuple((rowIndex, colIndex))
+					else: # Rows and columns are reversed here
+						cell = tuple((colIndex, rowIndex))
+					cost = self.matrix.get(cell)
+					# If not infinity, update cell's cost
+					if cost != None:
+						self.matrix[cell] = cost - minCost
+
+	# Ensures at least one zero on the rows and columns of the matrix (besides inf)
+	# Adjusts costSoFar as needed
+	def reduceCostOnMatrix(self):
+		self.findMinCostAndNormalize(row=True)
+		self.findMinCostAndNormalize(row=False)
+		print(self.matrix)
+
+	# Marks the city as visited, updates the matrix and then does reduceCost to normalize the matrix again
+	def visitCity(self, prevCity:City, cityToVisit:City):
+		self.unvisitedCitiesSet.remove(cityToVisit)
+
+		# Update cost after traveling to city
+		cost = self.matrix[tuple((prevCity, cityToVisit))]
+		self.costSoFar += cost
+		
+		# Remove all the impossible routes now
+		for i in range(len(self.cities)):
+			# Update matrix to remove all items from row prevCity
+			del self.matrix[tuple((prevCity, i))]
+
+			# Update matrix to remove all items from col cityToVisit
+			del self.matrix[tuple((i, cityToVisit))]
+			
+		# Update matrix to remove the inverse (cityToVisit -> prevCity) as well
+		del self.matrix[tuple((cityToVisit, prevCity))]
+
+		self.reduceCostOnMatrix()
+
+	def __str__(self) -> str:
+		string = "RouteSoFar:"
+		for city in self.routeSoFar:
+			string += f" -> {city._name}({city._index})"
+		string += "\nUnvisited Nodes: "
+		if len(self.unvisitedCitiesSet) != 0:
+			for city in self.unvisitedCitiesSet:
+				string += f"{city._name}({city._index}) "
+			# string += '\n'
+		else:
+			string += "*empty*\n"
+
+		# Format the matrix printing
+		table_data = [[]]
+		# Print the names of the cities at the top
+		for colIndex in range(-1, len(self.cities)-1):
+			if colIndex == -1:
+				table_data[0].append(" ")
+			else:
+				#assert(type(self.cities[i]) == City)
+				table_data[0].append(self.cities[colIndex]._name)
+		
+		for colIndex in range(0, len(self.cities)):
+			table_data.append([])
+			last = len(table_data)-1
+			for rowIndex in range(-1, len(self.cities)-1):
+				# Append the name of the city for the row
+				if rowIndex == -1:
+					table_data[last].append(self.cities[colIndex]._name)
+				# Print the same city -> same city as inf
+				elif rowIndex == colIndex:
+					table_data[last].append(INF_STRING)
+				# Lookup in dictionary and see if it exists
+				else:
+					cost = self.matrix.get(tuple((rowIndex, colIndex)))
+					if cost != None:
+						table_data[last].append(f"{cost}")
+					else:
+						table_data[last].append(INF_STRING)
+
+		formatString = "{: >5} "
+		for row in table_data:
+			for item in row:
+				string += formatString.format(item)
+			string += '\n'
+		string += '\n'
+
+		return string
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import signal
 import sys
 import time
 from Proj5GUI import Proj5GUI
+from TSPBranchAndBound import State, generateRootStateFromCities
+from TSPClasses import City, Scenario
 
 
 from which_pyqt import PYQT_VER
@@ -18,21 +20,68 @@ else:
 	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
-def test_should_solve_defaultRandom():
-  signal.signal(signal.SIGINT, signal.SIG_DFL)
+# def test_should_solve_defaultRandom():
+#   signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-  app = QApplication(sys.argv)
-  w = Proj5GUI()
-  # app.exec()
-  # assert(False)
-  w.generateNetwork()
-  w.solver.setupWithScenario(w._scenario)
-  
-  max_time = float(60)
-  results = w.solver.defaultRandomTour(max_time)
-  assert(results['cost'] < math.inf)
-  assert(results['solution'] is not None)
+#   app = QApplication(sys.argv)
+#   w = Proj5GUI()
+#   # app.exec()
+#   # assert(False)
+#   w.generateNetwork()
+#   w.solver.setupWithScenario(w._scenario)
+
+#   max_time = float(60)
+#   results = w.solver.defaultRandomTour(max_time)
+#   assert(results['cost'] < math.inf)
+#   assert(results['solution'] is not None)
   
 
 # def test_should_solve_greedy():
+#   w = Proj5GUI()
+#   w.diffDropDown.setCurrentIndex(4) # Hard (Deterministic)
+#   w.generateNetwork()
+#   w.solver.setupWithScenario(w._scenario)
+
+# Verifies that the initial State is set up correctly, with the matrix normalized and the costSoFar correct
+# (reference Homework 19)
+def test_init_state():
+  pass
+  scenario = [[math.inf, 7, 3, 12],
+              [3, math.inf, 6, 14],
+              [5, 8, math.inf, 6],
+              [9, 3, 5, math.inf]]
+  
+  correctStateMatrix = [[math.inf, 4, 0, 8],
+                        [0, math.inf, 3, 10],
+                        [0, 3, math.inf, 0],
+                        [6, 0, 2, math.inf]]
+  
+  # Because test is set to true, the cities will return the cost at an index of the double array
+  city1 = City(None, None, scenario=scenario, index=0, name='A', test=True)
+  city2 = City(None, None, scenario=scenario, index=1, name='B', test=True)
+  city3 = City(None, None, scenario=scenario, index=2, name='C', test=True)
+  city4 = City(None, None, scenario=scenario, index=3, name='D', test=True)
+  cities = [city1, city2, city3, city4]
+  
+  testState = generateRootStateFromCities(cities)
+
+  assert(len(testState.unvisitedCitiesSet) == len(cities))
+
+  # Assert that the matrix is equal at each value
+  for i in range(len(cities)):
+    for j in range(len(cities)):
+      correctCost = correctStateMatrix[i][j]
+      if correctCost == math.inf:
+        assert(testState.matrix.get(tuple((i, j))) == None)
+      else:
+        assert(testState.matrix.get(tuple((i, j))) == correctCost)
+
+  assert(testState.cities == cities)
+  assert(len(testState.routeSoFar) == 0)
+  assert(testState.costSoFar == 15)
+  assert(testState.isSolution() == False)
+  
+
+# def setScenario(w:Proj5GUI, points, diff, rand_seed):
+#   w._scenario = Scenario( city_locations=points, difficulty=diff, rand_seed=rand_seed )
 #   pass
