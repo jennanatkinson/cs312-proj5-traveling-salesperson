3f0e289e581d89e795c4d33d0384df14adae80f1
Jenna Atkinson <>
2022-11-28 23:42:27 -0700

    Uncomment out test

--- test.py
+++ test.py
@@ -20,20 +20,20 @@ else:
 	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
-# def test_should_solve_defaultRandom():
-#   signal.signal(signal.SIGINT, signal.SIG_DFL)
+def test_should_solve_defaultRandom():
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-#   app = QApplication(sys.argv)
-#   w = Proj5GUI()
-#   # app.exec()
-#   # assert(False)
-#   w.generateNetwork()
-#   w.solver.setupWithScenario(w._scenario)
+  app = QApplication(sys.argv)
+  w = Proj5GUI()
+  # app.exec()
+  # assert(False)
+  w.generateNetwork()
+  w.solver.setupWithScenario(w._scenario)
 
-#   max_time = float(60)
-#   results = w.solver.defaultRandomTour(max_time)
-#   assert(results['cost'] < math.inf)
-#   assert(results['solution'] is not None)
+  max_time = float(60)
+  results = w.solver.defaultRandomTour(max_time)
+  assert(results['cost'] < math.inf)
+  assert(results['solution'] is not None)
   
 
 # def test_should_solve_greedy():
