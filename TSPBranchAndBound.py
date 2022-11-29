2e1c376de2ae788e86c550ce1c97f6fe8c0113a8
Jenna Atkinson <>
2022-11-29 11:19:46 -0700

    Update infinity check and return to start methods

--- TSPBranchAndBound.py
+++ TSPBranchAndBound.py
@@ -7,26 +7,28 @@ INF_STRING = "-"
 
 # TODO: should prune if the column of 0 is all inf, should prune if the lower bound is bigger than the bssf
 
+# Keeps track/adjusts the current route, matrix and cost when adding new cities to the route
 class State:
-	#unvisitedCitiesSet, set of cities that haven't been visited yet
-	#matrix, dictionary to show which edges can be used next
-	#cities, reference to the list of cities
-	#routeSoFar, list of cities in order of what we have visited so far
-	#costSoFar, the cost we have accumulated on our route
-	def __init__(self, unvisitedSet:set=set(), matrix:dict=dict(), cities=[], route=[], costSoFar:int=0):
-		self.unvisitedCitiesSet = unvisitedSet
-		self.matrix = matrix
-		self.cities = cities
-		self.routeSoFar = route
-		self.costSoFar = costSoFar
-		self.isVisitStart = False
+	#unvisitedCitiesSet:set, set of cities that haven't been visited yet
+	#matrix:dict(tuple(rowIndex, colIndex):reducedCost), dictionary to show which edges can be used next (inf entries do not exist)
+	#cities:list[City], reference to the list of cities
+	#routeSoFar:list[City], list of cities in order of what we have visited so far
+	#costSoFar:int, the cost we have accumulated on our route
+	#isReturnVisitToStart:bool, if the route has finished through all the cities and returned back to the start
+	def __init__(self, unvisitedSet:set=set(), matrix:dict=dict(), cities:list[City]=[], route:list=[], costSoFar:int=0):
+		self.unvisitedCitiesSet:set = unvisitedSet
+		self.matrix:dict = matrix
+		self.cities:list[City] = cities
+		self.routeSoFar:list[City] = route
+		self.costSoFar:int = costSoFar
+		self._isReturnVisitToStart:bool = False # should ONLY be updated if len(unvisitedSet) == 0
 
 		# If given nothing but a list of cities, construct the root state
 		if len(cities) != 0 and costSoFar == 0 and len(matrix) == 0 and len(unvisitedSet) == 0 and len(route) == 0:
-			self.generateRootStateFromCities()
+			self._generateRootStateFromCities()
 
 	# Given a list of cities, return an init state
-	def generateRootStateFromCities(self):
+	def _generateRootStateFromCities(self):
 		# Unvisited cities should exclude the start node, so it is not revisited before the end
 		self.unvisitedCitiesSet = set(self.cities[1:])
 
@@ -44,42 +46,58 @@ class State:
 		self.matrix = matrix
 		self.reduceCostOnMatrix()
 
-	# # Returns true if there is a node that still can return to the starting city
-	# def isReturnToStart(self) -> bool:
-	# 	for i in range(len(self.cities)):
-	# 		if self.matrix.get(tuple((i, 0))) != None:
-	# 			return True
-	# 	return False
+	# Returns true if route is impossible or not going to yield a better result
+	# Should be used if branch is not a solution yet, but trying to determine if we should continue exploring or not
+	def shouldPrune(self, bssf:int=math.inf) -> bool:
+		return self.costSoFar == math.inf or self.costSoFar >= bssf
 
 	# Sees if this State yields a solution
 	def isSolution(self) -> bool:
-		return len(self.unvisitedCitiesSet) == 0 and self.costSoFar != math.inf and len(self.matrix) == 0
+		return len(self.unvisitedCitiesSet) == 0 and self.costSoFar != math.inf and len(self.matrix) == 0 and self._isReturnVisitToStart
 
 	# Return the route if a solution
+	# (will try to return to start if that is possible and hasn't been done yet)
 	def getSolution(self):
+		self._tryReturnToStart()
 		if self.isSolution():
 			return self.routeSoFar, self.costSoFar
 		else:
 			return None, None
 
-	# TODO: fix function
-	def isInfinity(self, city:City) -> bool: #row:bool, city:City) -> bool:
-		# if len(self.routeSoFar) <= 1:
-		# 	return False
-		# # If the first element in the route is 0, and the start hasn't been visited, then the first row will be inf, but not the column
-		# if city == self.routeSoFar[0] and not self.isVisitStart:
-		# 	return row
-		# # If it is the last element in the route, then only the column will be inf, not the row
-		# elif city == self.routeSoFar[len(self.routeSoFar)-1]:
-		# 	return not row
-		# else:
-		# 	return True
-		return False
+	# If all other nodes have been visited, try to return to the start from the last city (unless already returned)
+	# This will update the matrix and costSoFar if this is impossible
+	def _tryReturnToStart(self):
+		if self._isReturnVisitToStart: return
+		if len(self.unvisitedCitiesSet) == 0 and self.costSoFar != math.inf and len(self.matrix) == 0:
+			self.visitCity(self.cities[0])
 
-	# TODO: when pruning, should check to see the len of unvisited cities at the end. if it is zero, then make sure to add the remaining cost to get back to the start
-	# or set up a flag??
+	# Looks at the matrix and determines if a row or column will be inf (based on visit status)
+	def isInfinity(self, rowMajor:bool, city:City) -> bool:
+		# If the city has been visited (aka at least an edge inbound or outbound, which is the only way to be inf)
+		if not (city in self.unvisitedCitiesSet):
+			if len(self.routeSoFar) <= 1:
+				return False
+			# If the city is the startCity
+			if city == self.routeSoFar[0] and not self._isReturnVisitToStart:
+				# If the route has finished and returned back to the start, then the row and col will be inf
+				if self._isReturnVisitToStart:
+					return True
+				else:
+					# If startCity hasn't been visited, then the first row will be inf, but not the column
+					# print(f"skipping inf:startCity, rowMajor={rowMajor}")
+					return rowMajor
+			# If city == most recently visited in route, then only the column will be inf, not the row
+			elif city == self.routeSoFar[len(self.routeSoFar)-1]:
+				# print(f"skipping inf:lastCityVisited, rowMajor={rowMajor}")
+				return not rowMajor
+			# Otherwise, the city will have an outbound and an inbound edge already logged, so the col and row == inf
+			else:
+				# print(f"skipping inf:middleCityVisited, rowMajor={rowMajor}")
+				return True
+		else:
+			return False
 
-
+	# Generates the correct cell tuple based on if we are aiming for a row or a column in the loop (see findMinCostAndNormalize)
 	def _generateTuple(self, rowMajor:bool, i:int, j:int):
 		if rowMajor:
 			return tuple((i, j))
@@ -90,9 +108,8 @@ class State:
 	def findMinCostAndNormalize(self, rowMajor:bool=True):
 		# Look at row each row/column to find the minCost
 		for i in range(len(self.cities)):
-			print(f"i: {i}")
 			# If the whole row/column is going to be infinities, move on
-			if self.isInfinity(self.cities[i]):
+			if self.isInfinity(rowMajor, self.cities[i]):
 				continue
 			
 			minCost = math.inf
@@ -105,7 +122,6 @@ class State:
 					minCost = cost
 					if minCost == 0:
 						break
-				print(f"cell:{cell} mincost:{minCost}")
 			
 			# If we are taking on additional costs, we need to readjust the costSoFar and row/column numbers
 			if minCost != 0 and minCost < math.inf:
@@ -118,7 +134,7 @@ class State:
 					# If not infinity, update cell's cost
 					if cost != None:
 						self.matrix[cell] = cost - minCost
-			print(self)
+			# print(self)
 
 	# Ensures at least one zero on the rows and columns of the matrix (besides inf)
 	# Adjusts costSoFar as needed
@@ -128,21 +144,20 @@ class State:
 
 	# Marks the city as visited, updates the matrix and then does reduceCost to normalize the matrix again
 	def visitCity(self, cityToVisit:City):
+		if (self.costSoFar == math.inf or self._isReturnVisitToStart):
+			return
 		assert(len(self.routeSoFar) != 0)
 		prevCity = self.routeSoFar[len(self.routeSoFar)-1]
-		print(prevCity._index)
-
+		
 		# Ensure that there is actually a path to the other city
-		cost = self.matrix[tuple((prevCity._index, cityToVisit._index))]
+		cost = self.matrix.get(tuple((prevCity._index, cityToVisit._index)))
 		if cost == None:
+			self.costSoFar = math.inf #TODO: maybe change?
 			return
 		# Update cost after traveling to city
 		self.costSoFar += cost
-		
-		self.unvisitedCitiesSet.remove(cityToVisit)
-		self.routeSoFar.append(cityToVisit)
 
-		# Remove all the impossible routes now
+		# Remove all the impossible routes
 		for i in range(len(self.cities)):
 			# Update matrix to remove all items from row prevCity
 			self.matrix.pop(tuple((prevCity._index, i)), None)
@@ -153,12 +168,16 @@ class State:
 		# Update matrix to remove the inverse (cityToVisit -> prevCity) as well
 		self.matrix.pop(tuple((cityToVisit._index, prevCity._index)), None)
 
+		# Can only visit the startCity after visiting all other cities
 		if cityToVisit == self.cities[0]:
-			self.isVisitStart = True
+			assert(len(self.unvisitedCitiesSet) == 0)
 			assert(len(self.matrix) == 0)
-		
+			self._isReturnVisitToStart = True
 		else:
+			self.unvisitedCitiesSet.remove(cityToVisit)
+			self.routeSoFar.append(cityToVisit)
 			self.reduceCostOnMatrix()
+			
 
 	def __str__(self) -> str:
 		string = "State{"
