6ff0512dc042d4e74e59fdd144f31a0b915fb7d0
Jenna Atkinson <>
2022-11-30 22:02:08 -0700

    Update comments

--- TSPBranchAndBound.py
+++ TSPBranchAndBound.py
@@ -1,16 +1,13 @@
 import math
-
 from TSPClasses import City
 from copy import copy, deepcopy
 
-
 INF_STRING = "-"
 
+# Space: O(1)
 class PriorityEntry(object):
 	def __init__(self, numVisited:int, costSoFar:int, data):
 			self.data = data
-			# Negative so the biggerNum will be sorted first (reverse pQueue)
-			# self.numVisited:int = -abs(numVisited)
 			self.numVisited:int = numVisited
 			self.costSoFar:int = costSoFar
 
@@ -19,11 +16,11 @@ class PriorityEntry(object):
 				return self.costSoFar < other.costSoFar
 			return self.numVisited > other.numVisited
 
-# Keeps track/adjusts the current route, matrix and cost when adding new cities to the route
+# Keeps track/adjusts the current route, matrix and cost when adding new cities to the route, Space: O(n)
 class State:
 	#unvisitedCitiesSet:set, set of cities that haven't been visited yet
 	#matrix:dict(tuple(rowIndex, colIndex):reducedCost), dictionary to show which edges can be used next (inf entries do not exist)
-	#cities:list[City], reference to the list of cities
+	#cities:list[City], reference to the list of all cities
 	#routeSoFar:list[City], list of cities in order of what we have visited so far
 	#costSoFar:int, the cost we have accumulated on our route
 	#isReturnVisitToStart:bool, if the route has finished through all the cities and returned back to the start
@@ -47,7 +44,7 @@ class State:
 		# Route should include the start node
 		self.routeSoFar = [self.cities[0]]
 
-		#Initialize the matrix
+		#Initialize the matrix, Time: O(n**2)
 		matrix = dict()
 		for city in self.cities:
 			for otherCity in self.cities:
@@ -58,12 +55,12 @@ class State:
 		self.matrix = matrix
 		self._reduceCostOnMatrix()
 
-	# Returns true if route is impossible or not going to yield a better result
+	# Returns true if route is impossible or not going to yield a better result, Time: O(1)
 	# Should be used if branch is not a solution yet, but trying to determine if we should continue exploring or not
 	def shouldPrune(self, bssf:int=math.inf) -> bool:
 		return self.costSoFar == math.inf or self.costSoFar >= bssf
 
-	# Sees if this State yields a solution
+	# Sees if this State yields a solution, Time: O(1)
 	def isSolution(self) -> bool:
 		return len(self.unvisitedCitiesSet) == 0 and self.costSoFar != math.inf and len(self.matrix) == 0 and self._isReturnVisitToStart
 
@@ -83,7 +80,7 @@ class State:
 		if len(self.unvisitedCitiesSet) == 0 and self.costSoFar != math.inf:
 			self.visitCity(self.cities[0])
 
-	# Looks at the matrix and determines if a row or column will be inf (based on visit status)
+	# Looks at the matrix and determines if a row or column will be inf (based on visit status), Time: O(1)
 	def _isInfinity(self, rowMajor:bool, city:City) -> bool:
 		# If the city has been visited (aka at least an edge inbound or outbound, which is the only way to be inf)
 		if not (city in self.unvisitedCitiesSet):
@@ -94,39 +91,36 @@ class State:
 				# If the route has finished and returned back to the start, then the row and col will be inf
 				if self._isReturnVisitToStart:
 					return True
+				# If startCity hasn't been visited, then the first row will be inf, but not the column
 				else:
-					# If startCity hasn't been visited, then the first row will be inf, but not the column
-					# print(f"skipping inf:startCity, rowMajor={rowMajor}")
 					return rowMajor
 			# If city == most recently visited in route, then only the column will be inf, not the row
 			elif city == self.routeSoFar[len(self.routeSoFar)-1]:
-				# print(f"skipping inf:lastCityVisited, rowMajor={rowMajor}")
 				return not rowMajor
 			# Otherwise, the city will have an outbound and an inbound edge already logged, so the col and row == inf
 			else:
-				# print(f"skipping inf:middleCityVisited, rowMajor={rowMajor}")
 				return True
 		else:
 			return False
 
-	# Generates the correct cell tuple based on if we are aiming for a row or a column in the loop (see findMinCostAndNormalize)
+	# Generates the correct cell tuple based on if we are aiming for a row or a column in the loop (see findMinCostAndNormalize), Time: O(1)
 	def _generateTuple(self, rowMajor:bool, i:int, j:int):
 		if rowMajor:
 			return tuple((i, j))
 		else: # Rows and columns are reversed here
 			return tuple((j, i))
 	
-	# Ensures at least one zero on the rows/columns of the matrix (besides inf)
+	# Ensures at least one zero on the rows/columns of the matrix (besides inf), Time: O(n**2)
 	def _findMinCostAndNormalize(self, rowMajor:bool=True):
-		# Look at row each row/column to find the minCost
+		# Look at row each row/column to find the minCost, Time: O(n**2)
 		for i in range(len(self.cities)):
-			# If the whole row/column is going to be infinities, move on
+			# If the whole row/column is going to be infinities, move on, Time: O(1)
 			if self._isInfinity(rowMajor, self.cities[i]):
 				continue
 			
 			minCost = math.inf
 
-			#Examine each cell in a row/column to find the minCost
+			#Examine each cell in a row/column to find the minCost, Time: O(n)
 			for j in range(len(self.cities)):
 				cell = self._generateTuple(rowMajor, i, j)
 				cost = self.matrix.get(cell)
@@ -139,22 +133,20 @@ class State:
 			if minCost != 0 and minCost < math.inf:
 				self.costSoFar += minCost
 
-			# Update all costs in same row/column to normalize
+			# Update all costs in same row/column to normalize, Time: O(n)
 				for j in range(len(self.cities)):
 					cell = self._generateTuple(rowMajor, i, j)
 					cost = self.matrix.get(cell)
 					# If not infinity, update cell's cost
 					if cost != None:
 						self.matrix[cell] = cost - minCost
-			# print(self)
 
-	# Ensures at least one zero on the rows and columns of the matrix (besides inf)
-	# Adjusts costSoFar as needed
+	# Ensures at least one zero on the rows and columns of the matrix (besides inf), adjusts costSoFar as needed, Time: O(n**2)
 	def _reduceCostOnMatrix(self):
-		self._findMinCostAndNormalize(rowMajor=True)
-		self._findMinCostAndNormalize(rowMajor=False)
+		self._findMinCostAndNormalize(rowMajor=True)  # Time: O(n**2)
+		self._findMinCostAndNormalize(rowMajor=False) # Time: O(n**2)
 
-	# Marks the city as visited, updates the matrix and then does reduceCost to normalize the matrix again
+	# Marks the city as visited, updates the matrix and then does reduceCost to normalize the matrix again, Time: O(n**2)
 	def visitCity(self, cityToVisit:City):
 		if (self.costSoFar == math.inf or self._isReturnVisitToStart):
 			return
@@ -166,10 +158,11 @@ class State:
 		if cost == None:
 			self.costSoFar = math.inf
 			return
+
 		# Update cost after traveling to city
 		self.costSoFar += cost
 
-		# Remove all the impossible routes
+		# Remove all the impossible routes, Time: O(n)
 		for i in range(len(self.cities)):
 			# Update matrix to remove all items from row prevCity
 			self.matrix.pop(tuple((prevCity._index, i)), None)
@@ -180,7 +173,7 @@ class State:
 		# Update matrix to remove the inverse (cityToVisit -> prevCity) as well
 		self.matrix.pop(tuple((cityToVisit._index, prevCity._index)), None)
 
-		# Can only visit the startCity after visiting all other cities
+		# Only allowed to visit the startCity after visiting all other cities
 		if cityToVisit == self.cities[0]:
 			assert(len(self.unvisitedCitiesSet) == 0)
 			assert(len(self.matrix) == 0)
@@ -188,8 +181,9 @@ class State:
 		else:
 			self.unvisitedCitiesSet.remove(cityToVisit)
 			self.routeSoFar.append(cityToVisit)
-			self._reduceCostOnMatrix()
+			self._reduceCostOnMatrix() # Time: O(n**2)
 	
+	# Makes a shallow copy of all the elements (data structures are new, but if object, then reference is copied), Time: O(n)
 	def copy(self):
 		result = State()
 		result.unvisitedCitiesSet:set = set(self.unvisitedCitiesSet)
